7e9533a1df5f2a866982d7d5b84b68bcc1a26275
Сеитасанов Яхуб <seitasanov.yahub@gmail.com>
2023-02-04 16:59:56 +0300

    Added provider for preview environment

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,30 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 export default NextAuth({
   providers: [
-    GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
-    }),
+    process.env.VERCEL_ENV === 'preview'
+      ? CredentialsProvider({
+          name: 'Credentials',
+          credentials: {
+            username: {
+              label: 'Username',
+              type: 'text',
+              placeholder: 'jsmith',
+            },
+            password: { label: 'Password', type: 'password' },
+          },
+          authorize: async () => ({
+            id: '1',
+            name: 'J Smith',
+            email: 'jsmith@example.com',
+            image: 'https://i.pravatar.cc/150?u=jsmith@example.com',
+          }),
+        })
+      : GoogleProvider({
+          clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+          clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+        }),
   ],
 });
